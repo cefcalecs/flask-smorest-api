c9dead5a1b07521b2122501403d0a2285e0e85fa
Alexis Vintayen <alexis.vintayen@cefc.org.sg>
2023-03-10 17:47:21 +0800

    Changes: - add flask-migrate to requirements.txt - add a line in app.py to use flask-migrate and remove the SQLAlchemy create_all()

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 from blocklist import BLOCKLIST
@@ -25,7 +26,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
+    migrate = Migrate(app, db)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "cTvauuJ5MY6LjmAB"
@@ -89,9 +90,12 @@ def create_app(db_url=None):
             401,
         )
 
+    """
+        This is not needed anymore since we use flask_migrate
     @app.before_first_request
     def create_tables():
         db.create_all()
+    """
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
