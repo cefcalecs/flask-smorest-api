21e195937588bc0e4b3dfea824bfee2e1c2e3abd
Alexis Vintayen <alexis.vintayen@cefc.org.sg>
2023-03-10 12:14:49 +0800

    Changess: - added flask-jwt-extended and passlib in requirements.txt - added user model and loaded it - added UserSchema in schemas - added user resources - modified app to load jwt-extended and passlib

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
 import models
@@ -8,6 +9,7 @@ import models
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -25,6 +27,9 @@ def create_app(db_url=None):
 
     api = Api(app)
 
+    app.config["JWT_SECRET_KEY"] = "cTvauuJ5MY6LjmAB"
+    jwt = JWTManager(app)
+
     @app.before_first_request
     def create_tables():
         db.create_all()
@@ -32,5 +37,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
